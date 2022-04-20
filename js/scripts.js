d40da346bc6e6a03aefd8143206f83ec20dcb96c
Jesse Ilescas <ilesmj9516@gmail.com>
2022-04-19 20:06:41 -0500

    Incorporando DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let dato = 'Y'
 
-while (dato == 'Y') {
+while (dato != 'N') {
     
     const nombre = prompt('Ingrese el nombre completo del cliente')
     const direccion = prompt('Ingrese la dirección del cliente')
@@ -9,19 +9,35 @@ while (dato == 'Y') {
     let gananciaMensual = inversion * 0.125
     let interesCompuesto = gananciaMensual * 16
     let gananciaTotal = inversion + interesCompuesto
+
+    const datos =[ //se contruye objeto
+        {name: nombre, adress: direccion, inv: inversion, mens: gananciaMensual, compuesto: interesCompuesto, final: gananciaTotal}
+    ]
         function imprimirDatos () {
-            console.log('Cliente:', nombre)
-            console.log('Direccion:', direccion)
-            console.log('Cantidad invertida: $', inversion)
-            console.log('Interes Compuesto : $', interesCompuesto)
-            console.log('Interes mensual en pesos : $', gananciaMensual)
-            console.log('Monto final a cobrar: $', gananciaTotal)
-            console.log('Recuerda que el tiempo de trabajo son 16 meses(Valor expresado en M.N/MXN)')
-        }
+            const listaDatos = document.getElementsByTagName('ul')
+            for(const inf of datos){
+                const li = document.createElement('li')
+                li.innerHTML = `
+                <h3>Nombre: ${inf.name}</h3>
+                <h3>Direccion: ${inf.adress}</h3>
+                <h3>Inversion Inicial: ${inf.inv}</h3>
+                <h3>Ganancia Mensual: ${inf.mens}</h3>
+                <h3>Interes Compuesto: ${inf.compuesto}</h3>
+                <h3>Saldo Final: ${inf.final}</h3>
+                <h2>Recuerda que el tiempo de trabajo son 16 meses(Valor expresado en M.N/MXN)</h2>
+                `
+                listaDatos.appendChild(li)
+
+            }
+        //     console.log('Cliente:', nombre)
+        //     console.log('Direccion:', direccion)
+        //     console.log('Cantidad invertida: $', inversion)
+        //     console.log('Interes Compuesto : $', interesCompuesto)
+        //     console.log('Interes mensual en pesos : $', gananciaMensual)
+        //     console.log('Monto final a cobrar: $', gananciaTotal)
+        //     console.log('Recuerda que el tiempo de trabajo son 16 meses(Valor expresado en M.N/MXN)')
+        // }
         imprimirDatos()
     
-    if (dato == 'N') {
-        break
+        }
     }
-}
-
