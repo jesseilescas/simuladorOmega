71f6aa52fcbf84a14d4f884dcfeed0e4d95d80ee
Jesse Ilescas <ilesmj9516@gmail.com>
2022-04-21 18:34:52 -0500

    Agregando evento al simulador

--- js/scripts.js
+++ js/scripts.js
@@ -14,30 +14,28 @@ while (dato != 'N') {
         {name: nombre, adress: direccion, inv: inversion, mens: gananciaMensual, compuesto: interesCompuesto, final: gananciaTotal}
     ]
         function imprimirDatos () {
-            const listaDatos = document.getElementsByTagName('ul')
             for(const inf of datos){
-                const li = document.createElement('li')
-                li.innerHTML = `
+                const listaDatos = document.getElementById('listaDatos');
+                listaDatos.innerHTML = `
                 <h3>Nombre: ${inf.name}</h3>
                 <h3>Direccion: ${inf.adress}</h3>
-                <h3>Inversion Inicial: ${inf.inv}</h3>
-                <h3>Ganancia Mensual: ${inf.mens}</h3>
-                <h3>Interes Compuesto: ${inf.compuesto}</h3>
-                <h3>Saldo Final: ${inf.final}</h3>
+                <h3>Inversion Inicial: $ ${inf.inv}</h3>
+                <h3>Ganancia Mensual: $ ${inf.mens}</h3>
+                <h3>Interes Compuesto: $ ${inf.compuesto}</h3>
+                <h3>Saldo Final: $ ${inf.final}</h3>
                 <h2>Recuerda que el tiempo de trabajo son 16 meses(Valor expresado en M.N/MXN)</h2>
+                <button id="refrescarPagina" class="btn btn-dark">Calcular Nuevamene</button>
                 `
-                listaDatos.appendChild(li)
+             document.body.appendChild(listaDatos);
 
-            }
-        //     console.log('Cliente:', nombre)
-        //     console.log('Direccion:', direccion)
-        //     console.log('Cantidad invertida: $', inversion)
-        //     console.log('Interes Compuesto : $', interesCompuesto)
-        //     console.log('Interes mensual en pesos : $', gananciaMensual)
-        //     console.log('Monto final a cobrar: $', gananciaTotal)
-        //     console.log('Recuerda que el tiempo de trabajo son 16 meses(Valor expresado en M.N/MXN)')
-        // }
-        imprimirDatos()
-    
+            }    
         }
+        imprimirDatos()
+        
     }
+    const btn = document.getElementById('refrescarPagina')
+    btn.addEventListener('click', resfrescarPag)
+    function resfrescarPag(){
+        location.reload()
+    }
+    
